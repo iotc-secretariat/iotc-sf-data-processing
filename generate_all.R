509f5fd43c7c9fc9f0e7c744a04cba318d18e4ac
Emmanuel Chassot <EChassot@iotc.local>
2025-10-15 14:17:36 +0400

    Update of RC metadata and data export

--- generate_all.R
+++ generate_all.R
@@ -14,8 +14,8 @@ END_YEAR              = 2024
 
 # Select the URL for the document on conversion factors and morphometric relationships
 if(CODE_SPECIES_SELECTED %in% c("BET", "SKJ", "YFT")){
-  TITLE_CONVERSION_DOCUMENT = "IOTC-2023-WPTT25(AS)-DATA13"
-  URL_CONVERSION_DOCUMENT = "https://www.iotc.org/WPTT/25/Data/13-Equations"}
+  TITLE_CONVERSION_DOCUMENT = "IOTC-2025-WPTT27-DATA08"
+  URL_CONVERSION_DOCUMENT = "https://iotc.org/WPTT/27/Data/08-EQ"}
 
 if(CODE_SPECIES_SELECTED %in% c("BLT", "FRI", "KAW", "LOT", "COM", "GUT"))
    {
@@ -55,3 +55,22 @@ render("./rmd/SF_DESCRIPTION.Rmd", output_file = paste0("DESCRIPTION_", TITLE, "
        output_dir = "./outputs/docx/",
        params = list(dynamictitle = paste0("Standardisation of the size-frequency data for ", SPECIES_SELECTED, " ", START_YEAR, "--", END_YEAR))
 )
+
+# Temp RC METADATA
+
+setwd("initialisation/")
+source("90_LIBS.R")
+source("91_LIBS_EXTERNAL.R")
+#source("01_CL_EXTRACTION_RC.R")
+source("08_RC_DATA_EXPORT.R")
+setwd("..")
+
+render("./rmd/RC_METADATA.Rmd", 
+       output_format = "html_document2",
+       output_file = paste0("METADATA_", TITLE, ".html"), 
+       output_dir = "./outputs/html/",
+       params = list(dynamictitle = paste("METADATA FOR THE RETAINED DATA ")))
+
+
+
+
--- initialisation/08_RC_DATA_EXPORT.R
+++ initialisation/08_RC_DATA_EXPORT.R
@@ -1,96 +1,67 @@
-# Species of interest
-SPECIES_SELECTED = CL_SPECIES[SPECIES_CODE == CODE_SPECIES_SELECTED, SPECIES]
+#SPECIES_SELECTED = CL_SPECIES[SPECIES_CODE == CODE_SPECIES_SELECTED, SPECIES]
+SPECIES_SELECTED = "Bigeye tuna"
 
-TITLE = paste("IOTC-DATASETS-SF", CODE_SPECIES_SELECTED, min(FL_STD_DATA_SPECIES$YEAR), max(FL_STD_DATA_SPECIES$YEAR), sep = "-")
+#TITLE = paste("IOTC-DATASETS-RC", CODE_SPECIES_SELECTED, min(FL_STD_DATA_SPECIES$YEAR), max(FL_STD_DATA_SPECIES$YEAR), sep = "-")
+
+TITLE = paste("IOTC-DATASETS-RC", CODE_SPECIES_SELECTED, START_YEAR, END_YEAR, sep = "-")
+
+# Extract the data
+RC_BSE = NC_est(years = START_YEAR:END_YEAR, species_codes = CODE_SPECIES_SELECTED, factorize_results = TRUE)[, .(CATCH = sum(CATCH)), keyby = .(YEAR, FLEET_CODE, FLEET, FISHERY_TYPE_CODE, FISHERY_TYPE, FISHERY_GROUP_CODE, FISHERY_GROUP, GEAR_CODE, GEAR, FISHERY_CODE, FISHERY, FISHING_GROUND_CODE, FISHING_GROUND, SPECIES_CATEGORY, SPECIES_CATEGORY_CODE, SPECIES_CODE, SPECIES, SPECIES_SCIENTIFIC, IS_IOTC_SPECIES, IS_SPECIES_AGGREGATE, IS_SSI, FATE_TYPE_CODE, FATE_TYPE, FATE_CODE, FATE, CATCH_UNIT_CODE)]
+                                                                                                          
+# Export the file
+write.csv(RC_BSE, paste0("../outputs/data/", TITLE, ".csv"))
+
+# File size in KB
+RC_BSE_DATA_SIZE = round(file.size(paste0("../outputs/data/", TITLE, ".csv"))/1e3)
 
 # EXPORT FIELD AND CODELISTS ####
 
 # Create empty workbook
-STD_SF_DATA = createWorkbook("STD_SF_DATA")
+RC_BSE_DATA = createWorkbook("BSE_RC_DATA")
 
 # DATA FIELDS ####
 
 # Read description of data fields
 
 ## Wide table format
-FIELDS_STD_RC_DATA_TABLE = data.table(read.xlsx("../inputs/FIELDS_STANDARDIZED_RC_DATA.xlsx", sheet = "RC FORMAT FIELDS", sep.names = " "))[, .(FIELD, DEFINITION, `CODE LIST NAME`, `CODE LIST URL`)]
+FIELDS_RC_BSE_DATA_TABLE = data.table(read.xlsx("../inputs/FIELDS_RC_DATA.xlsx", sheet = "RC FORMAT FIELDS", sep.names = " "))[FIELD %in% names(RC_BSE), .(FIELD, DEFINITION, `CODE LIST NAME`, `CODE LIST URL`)]
 
-FIELDS_STD_RC_DATA_TABLE[!is.na(`CODE LIST NAME`), `CODE LIST` := paste0("<a href=\"", `CODE LIST URL`, "\">", `CODE LIST NAME`, "</a>")]
+FIELDS_RC_BSE_DATA_TABLE[!is.na(`CODE LIST NAME`), `CODE LIST` := paste0("<a href=\"", `CODE LIST URL`, "\">", `CODE LIST NAME`, "</a>")]
 
-FIELDS_STD_RC_DATA_TABLE = FIELDS_STD_RC_DATA_TABLE[, -c("CODE LIST NAME", "CODE LIST URL")]
-
-# Melted table format
-FIELDS_STD_SF_DATA = data.table(read.xlsx("../inputs/FIELDS_STANDARDIZED_SF_DATA.xlsx", sheet = "MELTED FORMAT FIELDS - CURRENT", sep.names = " "))[, .(FIELD, DEFINITION, `CODE LIST NAME`, `CODE LIST URL`)]
+FIELDS_RC_BSE_DATA_TABLE = FIELDS_RC_BSE_DATA_TABLE[, -c("CODE LIST NAME", "CODE LIST URL")]
 
 # Add to workbook
-addWorksheet(STD_SF_DATA, "WIDE FORMAT FIELDS")
-writeDataTable(STD_SF_DATA, sheet = "WIDE FORMAT FIELDS", x = FIELDS_STD_SF_DATA_TABLE, rowNames = FALSE, tableStyle = "TableStyleLight11")
-setColWidths(STD_SF_DATA, sheet = "WIDE FORMAT FIELDS", cols = 1:ncol(FIELDS_STD_SF_DATA_TABLE), widths = "auto")
+addWorksheet(RC_BSE_DATA, "field_description")
+writeDataTable(RC_BSE_DATA, sheet = "field_description", x = FIELDS_RC_BSE_DATA_TABLE, rowNames = FALSE, tableStyle = "TableStyleLight11")
+setColWidths(RC_BSE_DATA, sheet = "field_description", cols = 1:ncol(FIELDS_RC_BSE_DATA_TABLE), widths = "auto")
 
-addWorksheet(STD_SF_DATA, "MELTED FORMAT FIELDS")
-writeDataTable(STD_SF_DATA, sheet = "MELTED FORMAT FIELDS", x = FIELDS_STD_SF_DATA, rowNames = FALSE, tableStyle = "TableStyleLight11")
-setColWidths(STD_SF_DATA, sheet = "MELTED FORMAT FIELDS", cols = 1:ncol(FIELDS_STD_SF_DATA), widths = "auto")
-
-# STANDARDIZED SF DATA ####
-
-# CODE LISTS ####
-
-## FLEETS ####
-addWorksheet(STD_SF_DATA, "FLEETS")
-writeDataTable(STD_SF_DATA, sheet = "FLEETS", x = CL_FLEETS,  tableStyle = "TableStyleLight11")
-setColWidths(STD_SF_DATA, sheet = "FLEETS", cols = 1:nrow(CL_FLEETS), widths = "auto")
-
-## IRREGULAR GRID ####
-addWorksheet(STD_SF_DATA, "FISHING GROUNDS")
-writeDataTable(STD_SF_DATA, sheet = "FISHING GROUNDS", x = CL_FISHING_GROUNDS,  tableStyle = "TableStyleLight11")
-setColWidths(STD_SF_DATA, sheet = "FISHING GROUNDS", cols = 1:nrow(CL_FISHING_GROUNDS), widths = "auto")
-
-## GEARS ####
-addWorksheet(STD_SF_DATA, "GEARS")
-writeDataTable(STD_SF_DATA, sheet = "GEARS", x = CL_GEARS,  tableStyle = "TableStyleLight11")
-setColWidths(STD_SF_DATA, sheet = "GEARS", cols = 1:nrow(CL_GEARS), widths = "auto")
-
-## SCHOOL TYPES ####
-addWorksheet(STD_SF_DATA, "SCHOOL TYPES")
-writeDataTable(STD_SF_DATA, "SCHOOL TYPES", CL_SCHOOL_TYPES, tableStyle = "TableStyleLight11")
-setColWidths(STD_SF_DATA, sheet = "SCHOOL TYPES", cols = 1:nrow(CL_SCHOOL_TYPES), widths = "auto")
-
-## MEASUREMENT TYPES ####
-addWorksheet(STD_SF_DATA, "MEASUREMENTS")
-writeDataTable(STD_SF_DATA, "MEASUREMENTS", CL_MEASUREMENTS, tableStyle = "TableStyleLight11")
-setColWidths(STD_SF_DATA, sheet = "MEASUREMENTS", cols = 1:nrow(CL_MEASUREMENTS), widths = "auto")
-
-## RAISINGS ####
-addWorksheet(STD_SF_DATA, "RAISINGS")
-writeDataTable(STD_SF_DATA, "RAISINGS", CL_RAISINGS, tableStyle = "TableStyleLight11")
-setColWidths(STD_SF_DATA, sheet = "RAISINGS", cols = 1:nrow(CL_RAISINGS), widths = "auto")
-
+# # CODE LISTS ####
+# 
+# ## FLEETS ####
+# addWorksheet(BSE_RC_DATA, "FLEETS")
+# writeDataTable(BSE_RC_DATA, sheet = "FLEETS", x = CL_FLEETS, tableStyle = "TableStyleLight11")
+# setColWidths(BSE_RC_DATA, sheet = "FLEETS", cols = 1:nrow(CL_FLEETS), widths = "auto")
+# 
+# ## IRREGULAR GRID ####
+# addWorksheet(BSE_RC_DATA, "FISHING GROUNDS")
+# writeDataTable(BSE_RC_DATA, sheet = "FISHING GROUNDS", x = CL_FISHING_GROUNDS,  tableStyle = "TableStyleLight11")
+# setColWidths(BSE_RC_DATA, sheet = "FISHING GROUNDS", cols = 1:nrow(CL_FISHING_GROUNDS), widths = "auto")
+# 
+# ## GEARS ####
+# addWorksheet(BSE_RC_DATA, "GEARS")
+# writeDataTable(BSE_RC_DATA, sheet = "GEARS", x = CL_GEARS,  tableStyle = "TableStyleLight11")
+# setColWidths(BSE_RC_DATA, sheet = "GEARS", cols = 1:nrow(CL_GEARS), widths = "auto")
+# 
+# ## SCHOOL TYPES ####
+# addWorksheet(BSE_RC_DATA, "SCHOOL TYPES")
+# writeDataTable(BSE_RC_DATA, "SCHOOL TYPES", CL_SCHOOL_TYPES, tableStyle = "TableStyleLight11")
+# setColWidths(BSE_RC_DATA, sheet = "SCHOOL TYPES", cols = 1:nrow(CL_SCHOOL_TYPES), widths = "auto")
+# 
 # Save the workbook
-saveWorkbook(STD_SF_DATA, paste0("../outputs/", TITLE, "-DDD.xlsx"), overwrite = TRUE)
-
-# EXPORT DATA AS CSV FILE ####
-
-## MELTED FORMAT ####
-write.csv(FL_STD_DATA_SPECIES, file = paste0("../outputs/", TITLE, "-MELTED-FORMAT.csv"), row.names = FALSE)
-
-# File size in KB
-STD_SF_MELTED_DATA_SIZE = round(file.size(paste0("../outputs/", TITLE, "-MELTED-FORMAT.csv"))/1e3)
-
-## WIDE FORMAT ####
-write.csv(FL_STD_DATA_SPECIES_TABLE, file = paste0("../outputs/", TITLE, "-WIDE-FORMAT.csv"), row.names = FALSE)
-
-# File size in KB
-STD_SF_TABLE_DATA_SIZE = round(file.size(paste0("../outputs/", TITLE, "-WIDE-FORMAT.csv"))/1e3)
+saveWorkbook(RC_BSE_DATA, paste0("../outputs/data/", TITLE, "-DDD.xlsx"), overwrite = TRUE)
 
 # Zip the files
-zip::zip(zipfile = paste0("../outputs/", TITLE, ".zip"), files = c(paste0("../outputs/", TITLE, "-WIDE-FORMAT.csv"), paste0("../outputs/", TITLE, "-MELTED-FORMAT.csv"), paste0("../outputs/", TITLE, "-DDD.xlsx")), mode = "cherry-pick", recurse = FALSE)
-         
+zip::zip(zipfile = paste0("../outputs/data/", TITLE, ".zip"), files = c(paste0("../outputs/data/", TITLE, ".csv"), paste0("../outputs/data/", TITLE, "-DDD.xlsx")), mode = "cherry-pick", recurse = FALSE)
+
 # Zipped file size in KB
-STD_SF_DATA_ZIP_SIZE = round(file.size(paste0("../outputs/", TITLE, ".zip"))/1e3)
-
-# Remove files after creating the archive
-file.remove(paste0("../outputs/", TITLE, "-DDD.xlsx"))
-file.remove(paste0("../outputs/", TITLE, "-MELTED-FORMAT.csv"))
-file.remove(paste0("../outputs/", TITLE, "-WIDE-FORMAT.csv"))
-
-
+RC_BSE_DATA_ZIP_SIZE = round(file.size(paste0("../outputs/data/", TITLE, ".zip"))/1e3)
